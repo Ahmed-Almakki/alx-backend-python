c64a109b765637ad0291f58010e1b5d94616d839
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-10-11 00:44:54 +0000

    return floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""type-annotated function floor which takes a float n"""
+from math import floor as flor
+
+
+def floor(n: float) -> int:
+    """ return floor of paramter"""
+    return flor(n)
