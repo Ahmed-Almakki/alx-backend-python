672b4e8db87ff504b264cf3de07aafe0836a58de
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-10-14 23:28:01 +0000

    use asyncio.complete

--- 0x01-python_async_function/4-task.py
+++ 0x01-python_async_function/4-task.py
@@ -7,7 +7,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ return the list of all the delays"""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     res = []
