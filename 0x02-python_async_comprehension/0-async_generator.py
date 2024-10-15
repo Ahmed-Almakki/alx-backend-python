15d63deba9253449b7a88813bb7b63ec917a3356
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-10-15 18:58:10 +0000

    first commit

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""coroutine called async_generator that takes no arguments"""
+import asyncio
+from random import uniform
+from typing import AsyncIterator
+
+
+async def async_generator() -> AsyncIterator[float]:
+    """coroutine will loop 10 times, each time asynchronously wait 1 second"""
+    for _ in range(10):
+        numb = uniform(0, 10)
+        await asyncio.sleep(1)
+        yield numb
